5a665f618f99928405c668030d73e1c14f0d9b27
Artur Akulich <>
2024-09-18 16:06:04 +0300

    Initial commit

---
+++ Fancy Cells/Fancy Cells/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Fancy Cells
+//
+//  Created by Artur Akulich on 18.09.24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
